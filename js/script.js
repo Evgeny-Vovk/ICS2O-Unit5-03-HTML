481c0376d89eb0d9745b26f4490a3bc4ce871e42
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-19 16:59:48 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Created by: Evgeny
+// Created on: may 2022
+// This file contains the JS functions for index.html
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-03-HTML/",
+  })
+}
+
+/**
+ * This function compares slider with random number.
+ */
+
+function calculate() {
+
+  //input
+  const number = parseInt(document.getElementById('number').value)
+
+  // process
+  if (number >= 0) {
+    document.getElementById("answer").innerHTML =
+      "The number " + number + " is a positive number."
+  }
+  else {
+    document.getElementById("answer").innerHTML =
+      "The number " + number + " is a negative number."
+  }
+}
